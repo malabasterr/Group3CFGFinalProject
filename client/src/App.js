734a494c6caa6c4e41693845c2b43d8094b7252c
ecadogan <69811404+ecadogan@users.noreply.github.com>
2023-08-04 12:05:15 +0100

    fixed merges

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'; //Bootstrap import
 import { Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Error from './components/Error';
-<<<<<<< Updated upstream
-
-function App() {
-    return (
-      <main>
-=======
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-import Homepage from './components/Homepage';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 
@@ -32,17 +25,6 @@ function App() {
       <FooterComponent />
     </main>
   );
-}
->>>>>>> Stashed changes
-
-        <Routes >
-          <Route path="/" element={<Homepage />} />
-          {/* Routes to other pages would go here */}
-          <Route path="*" element={<Error />} />
-        </Routes>
-
-      </main>
-    );
 };
 
 export default App
