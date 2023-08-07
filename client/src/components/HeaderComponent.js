4c9ab22247bfa5d1a71799c561d88b6edc34e2c5
ecadogan <69811404+ecadogan@users.noreply.github.com>
2023-08-07 17:20:22 +0100

    Edited and removed files and Homepage work
    I removed some files from the original react downlaod which weren't needed.
    
    I have also added some css to the homepage need to check details on bootstrap for React and How to use hooks for nav linking

--- client/src/components/HeaderComponent.js
+++ client/src/components/HeaderComponent.js
@@ -6,12 +6,12 @@ function HeaderComponent() {
       <Container>
         <Row className="w-100">
           <Col xs={8} md={4}>
-            <Navbar.Brand href="#home">Logo</Navbar.Brand>
+            <Navbar.Brand href="#home">INYIM</Navbar.Brand>
           </Col>
           <Col xs={4} md={8}>
             <Nav className="justify-content-end">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Login</Nav.Link>
+              <Nav.Link href="Login.js">Login</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
             </Nav>
           </Col>
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,29 +1,21 @@
 import React, { useState } from 'react';
 import Login from './Login'
 import './Homepage.css';
-<link href="https://fonts.googleapis.com/css?family=Bungee+Inline" rel="stylesheet"></link>
 
 function Homepage() {
 
   return (
     <div>
+      <div>
         <h1>It's Not you It's Me!</h1>
-        
-          <div className="container">
-            <div className="row">
-              <div className="col col-4">
-                {<img src={process.env.PUBLIC_URL + '/Dragons.png'} alt="Dragon IMG" />}
-              </div>
-            <div className="col col-4">
-                {<Login></Login>}
+            
+            <div className='login'>
+              {<Login></Login>}
             </div>
-            <div className="col col-4">
-                {<img src={process.env.PUBLIC_URL + '/Dragons.png'} alt="Dragon IMG" />}
-            </div>
-        </div>
-    </div>
+
+      </div>
    
-        <div>
+        <div className='howToPlay'>
             <h2>How To Play</h2>
         </div>
 
