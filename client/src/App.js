f439d793966ac125ddba314a16916809e65bf5fc
ecadogan <69811404+ecadogan@users.noreply.github.com>
2023-08-18 17:39:50 +0100

    Fixed Merge issues

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Error from './components/Error';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import HeaderComponent from './components/HeaderComponent';
-import FooterComponent from './components/FooterComponent';
+import LoginSignup from './components/SigninLoginPage.js';
+import HeaderComponent from './components/header/HeaderComponent.js';
+import FooterComponent from './components/footer/FooterComponent.js';
 import QuizSlider from './components/QuizSlider';
 import './components/QuizSlider.css'
 
@@ -23,8 +22,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/SigninLoginPage.js" element={<LoginSignup />} />
         {/* This is not a real path, I am using it to test my features */}
-        <Route path="/RoomHome" element={<RoomHome />} />
-        <Route path="/room/:id" element={<Room />} />
+        {/* <Route path="/RoomHome" element={<RoomHome />} />
+        <Route path="/room/:id" element={<Room />} /> */}
         {/* Routes to other pages will go here */}
         <Route path="*" element={<Error />} />
       </Routes>
