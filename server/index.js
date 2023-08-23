2b3bc474e725f3eba4b0c8e3cd93ae7a830f02f0
malabasterr <113557081+malabasterr@users.noreply.github.com>
2023-08-23 14:48:58 +0100

    update package.json

--- server/index.js
+++ server/index.js
@@ -18,11 +18,12 @@ const io = new Server(httpServer, {
 });
 
 const rooms = [];
+const cachedQuestions = null;
 
 // Game Socket API
 io.on("connection", (socket) => {
   console.log("connected", socket.id);
-  roomHandler(io, socket, rooms);
+  roomHandler(io, socket, rooms, cachedQuestions);
 
   socket.on("disconnect", () => {
     console.log("disconnected", socket.id);
@@ -36,6 +37,34 @@ app.get('/api/questions/random', async (req, res) => {
   res.json({ questions: questionSet });
 });
 
+// // Defining the index and the max amount of questions. 
+// let lastQuestionIndex = 0;
+// const maxQuestions = 10;
+
+// // Function that shuffles the questions array using the Fisher-Yates shuffle algorithm
+// function shuffleArray(array) {
+// for (let i = array.length - 1; i > 0; i--) {
+//   const j = Math.floor(Math.random() * (i + 1));
+//   [array[i], array[j]] = [array[j], array[i]];
+// }
+// }
+
+// shuffleArray(questions);
+
+// // API route to retrieve a set of shuffled questions
+// app.get('/api/questions/random', (req, res) => {
+// //This is selecting the first 10 Questions in the shuffled array.
+// const questionSet = questions.slice(lastQuestionIndex, maxQuestions);
+// //This is the format passed to the QuizSlider component
+// res.json({ questions: questionSet });
+// });
+
+// // API route to reshuffle and retrieve questions
+// app.get('/api/questions/reshuffle', (req, res) => {
+//   shuffleArray(questions); // Reshuffle the questions array
+//   const questionSet = questions.slice(lastQuestionIndex, maxQuestions);
+//   res.json({ questions: questionSet });
+// });
 
 const port = process.env.PORT || 1234;
 httpServer.listen(port, () => console.log(`Listening on port ${port}`));
